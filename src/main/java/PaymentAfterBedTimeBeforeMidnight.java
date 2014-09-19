359731b94968bad91e11ad9186b32aec768aebda
na5j2t4 <nathan_halterman@gap.com>
2014-09-19 16:46:20 -0400

    removing unused files

--- src/main/java/PaymentAfterBedTimeBeforeMidnight.java
+++
@@ -1,9 +0,0 @@
-
-public class PaymentAfterBedTimeBeforeMidnight implements PaymentHours {
-
-	@Override
-	public int calculate(int startTime, int endTime, int bedTime) {
-		return (endTime - startTime) * 8;
-	}
-
-}
--- src/main/java/PaymentBeforeBedTime.java
+++
@@ -1,9 +0,0 @@
-
-public class PaymentBeforeBedTime implements PaymentHours {
-
-	@Override
-	public int calculate(int startTime, int endTime, int bedTime) {
-		return (endTime - startTime) * 12;
-	}
-
-}

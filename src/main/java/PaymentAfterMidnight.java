b5fb95aa6aff02ba02e99ee0a36af1a9aa949795
scoobycoder <scoobycoder@gmail.com>
2014-09-19 16:34:07 -0400

    Delete PaymentAfterMidnight.java

--- src/main/java/PaymentAfterMidnight.java
+++
@@ -1,9 +0,0 @@
-
-public class PaymentAfterMidnight implements PaymentHours {
-
-	@Override
-	public int calculate(int startTime, int endTime, int bedTime) {
-		return (endTime - startTime) * 16;
-	}
-
-}
